abd2a9e9b76a155a93f87e538d479e6044f50866
Anastasiya Cherkasova <cherkasovaam@bk.ru>
2023-06-14 03:08:56 +0300

    2nd try

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -6,7 +6,7 @@ from posts.models import Post, Group, Comment
 class CommentSerializer(serializers.ModelSerializer):
     author = serializers.SlugRelatedField(read_only=True,
                                           slug_field='username')
-
+    post = serializers.PrimaryKeyRelatedField(read_only=True)
     class Meta:
         model = Comment
         fields = '__all__'
